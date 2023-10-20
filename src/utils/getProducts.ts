4bb885aa984d6a1d38af9b5eaf9bfb7c1d35ea5c
Elhaam <e4elhaam@gmail.com>
2023-10-21 01:46:52 +0500

    ui additions

---
+++ src/utils/getProducts.ts
@@ -0,0 +1,15 @@
+// export async function storeFront(query, variables = {}){
+//     const res = await fetch(
+//         API_URL,
+//         {
+//             method: 'POST',
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "X-Shopify-Storefront-Access-Token": ACCESS_TOKENX
+//             }
+//         },
+//         body: JSON.stringify({query, variables})
+//     )
+
+//     return res.json();
+// }
